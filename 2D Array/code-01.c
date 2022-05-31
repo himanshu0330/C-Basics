634df2fe79aa592ca4c1815dfebdc5a6788d8d04
himanshu.agrawal_cs.aiml21@gla.ac.in <92987995+its-himanshu-13@users.noreply.github.com>
2022-05-31 13:41:45 +0530

    Create code-01.c

---
+++ 2D Array/code-01.c
@@ -0,0 +1,23 @@
+//Creating 2d array by user elements and printing them all
+
+#include<stdio.h>
+int main(){
+   int disp[2][3];
+   int i, j;
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("Enter value for disp[%d][%d]:", i, j);
+         scanf("%d", &disp[i][j]);
+      }
+   }
+   printf("Two Dimensional array elements:\n");
+   for(i=0; i<2; i++) {
+      for(j=0;j<3;j++) {
+         printf("%d ", disp[i][j]);
+         if(j==2){
+            printf("\n");
+         }
+      }
+   }
+   return 0;
+}
