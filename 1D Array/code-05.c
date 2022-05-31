2fd161bee81f315b24e10f0c88f7f9f52a749a2e
himanshu.agrawal_cs.aiml21@gla.ac.in <92987995+its-himanshu-13@users.noreply.github.com>
2022-05-31 13:37:38 +0530

    Create code-05.c

---
+++ 1D Array/code-05.c
@@ -0,0 +1,46 @@
+//C program to count frequency of each element of array
+ 
+
+#include <stdio.h>
+
+int main()
+{
+    int arr[100], freq[100];
+    int size, i, j, count;
+    printf("Enter size of array: ");
+    scanf("%d", &size);
+    printf("Enter elements in array: ");
+    for(i=0; i<size; i++)
+    {
+        scanf("%d", &arr[i]);
+        freq[i] = -1;
+    }
+
+
+    for(i=0; i<size; i++)
+    {
+        count = 1;
+        for(j=i+1; j<size; j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                count++;
+                freq[j] = 0;
+            }
+        }
+        if(freq[i] != 0)
+        {
+            freq[i] = count;
+        }
+    }
+    printf("\nFrequency of all elements of array : \n");
+    for(i=0; i<size; i++)
+    {
+        if(freq[i] != 0)
+        {
+            printf("%d occurs %d times\n", arr[i], freq[i]);
+        }
+    }
+
+    return 0;
+}
