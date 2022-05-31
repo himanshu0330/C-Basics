6f697937b764fe0cafeaac60a1cd31558dd46793
himanshu.agrawal_cs.aiml21@gla.ac.in <92987995+its-himanshu-13@users.noreply.github.com>
2022-05-31 13:54:02 +0530

    Create code-03.c

---
+++ 2D Array/code-03.c
@@ -0,0 +1,26 @@
+//Printing a specific pattern
+
+#include <stdio.h>
+int main()
+{
+    int i,j,a[15][15],n;
+    printf("enter no. of lines:");
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=i;j++)
+        {
+            if (j==0||i==j)
+                a[i][j]=1;
+            else
+                a[i][j]=a[i-1][j-1]+a[i-1][j];
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=0;j<=i;j++)
+            printf("%d",a[i][j]);
+        printf("\n");
+    }
+    return 0;
+}
