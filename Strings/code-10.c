6df5d1b87a62f9f991adf489ee551a2954d3e416
himanshu.agrawal_cs.aiml21@gla.ac.in <92987995+its-himanshu-13@users.noreply.github.com>
2022-05-31 23:02:41 +0530

    Update code-10.c

--- Strings/code-10.c
+++ Strings/code-10.c
@@ -1,24 +1,16 @@
-//TO COUNT THE NUMBER OF VOWELS, CONSONENTS,DIGITS AND SPECIAL CHARACTER IN THE STRING:
+// TO CONCATENATE TWO STRING:
 
 #include <stdio.h>
 
 int main()
 {
-    char a[20];
-    scanf(“%[^\n]&*c”,a);
-    int vow=0,conso=0,digi=0,spech=0;
-    for(int i=0;a[i];i++){
-    if(65<=a[i] && a[i]<=90 || 97<=a[i] && a[i]<=122){
-        if(a[i]=='a'||a[i]=='A'||a[i]=='i'||a[i]=='I'||a[i]=='o'||a[i]=='O'||a[i]=='u'||a[i]=='U'||a[i]=='E'||a[i]=='e')
-        vow++;
-        else
-        conso++;
-    }
-    else if(48<=a[i] && a[i]<=57)
-    digi++;
-    else
-    spech++;}
-    printf("vowels= %d\nconsonants= %d\ndigit=%d\nspecial charater= %d",vow,conso,digi,spech);
-
-    return 0;
-}
+    char a[]={"hello"}; //first string
+    char b[]={"GlA"};   //second string
+    char c[20];         //third string
+    int i;
+    for(i=0;a[i];i++)
+    c[i]=a[i];              //first in third
+    for(int j=0;b[j];j++)
+    c[i++]=b[j];   //second in third
+    c[i]=0;
+    printf("%s",c);
